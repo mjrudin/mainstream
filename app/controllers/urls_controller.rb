e1250bb1f96a6b3265cc34281ef8df86170fe3d3
Matthew Rudin <mjrudin@gmail.com>
2013-09-08 00:51:55 -0400

    Eliminates ask.fm links and updates css.

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,8 +1,8 @@
 class UrlsController < ApplicationController
   
   def index
-    @articles = Url.all
     tweetfetch
+    @articles = Url.all
   end
   
   def show
@@ -11,25 +11,29 @@ class UrlsController < ApplicationController
   end
   
 
-
+  #fetches links (allowed 15 req every 15 min), stores in db, gets articles for tweets
   def tweetfetch
    @array = []
    TweetStream::Client.new.sample(language: 'en') do |status, client|
      link = status.text.match(/http:\/\/t\.co\/[a-zA-Z0-9]+/)
+     #continue if tweet has a link in it
     if link
       @array.push(link[0])
 
+      #use this if we need to get hashtags later on
        # status.text.scan(/#\w+/).each do |h|
 #          Hashtag.create(tag: h.sub!("#", ""), url_id: Url.last.id)
 #        end
      end
-     client.stop if @array.length >= 15
+     #continue streaming until we have 50 links
+     client.stop if @array.length >= 50
   end
 
    @array.each do |link|
      until link.nil?
        final_link = link
        isSSL = link.match(/https:\/\//)
+       #for https links
        if isSSL
          uri = URI.parse(link)
          http = Net::HTTP.new(uri.host, uri.port)
@@ -38,18 +42,30 @@ class UrlsController < ApplicationController
          request = Net::HTTP::Get.new(uri.request_uri)
          response = http.request(request)
          link = response['location']
+       #for http links
        else
          link = Net::HTTP.get_response(URI.parse(link))['location']
        end
      end
      
+     #gets rid of ask.fm, twitter, and facebook links
+     isAskFM = final_link.match(/.*ask\.fm/)
+         isTwitter = final_link.match(/.*twitter\.com/)
+         isFacebook = final_link.match(/.*facebook\.com/)
+      
+         if isAskFM || isTwitter || isFacebook
+           next
+         end
+     
+     
+     #doesn't work
      l = Url.find_or_create_by_link(final_link) do |c|
            c.frequency += 1
          end
          l.frequency += 1
    end
 
-   
+   #uses readability to get article and meta data
    Url.all.each do |u|
      url = 'https://readability.com/api/content/v1/parser?url=' + "#{u.link}" + '&token=b9a8aa67a490c643bd12efd3d861c05a2a665927'
      uri = URI.parse(url)
